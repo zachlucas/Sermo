1196e5780998327aaffdd5fabcd4d4579a682552
Zach Lucas <zach.lucas@smallplanet.com>
2014-05-08 17:04:24 -0400

    Starting firebase chat

---
+++ Sermo/ChatViewController.m
@@ -0,0 +1,177 @@
+//
+//  ViewController.m
+//  Firechat
+//
+//  Copyright (c) 2012 Firebase.
+//
+//  No part of this project may be copied, modified, propagated, or distributed
+//  except according to terms in the accompanying LICENSE file.
+//
+
+#import "ChatViewController.h"
+#import <Parse/Parse.h>
+
+
+#define kFirechatNS @"https://myapp.firebaseIO-demo.com/"
+
+@implementation ChatViewController
+
+@synthesize nameField;
+@synthesize textField;
+@synthesize tableView;
+
+#pragma mark - Setup
+
+// Initialization.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+    // Initialize array that will store chat messages.
+    self.chat = [[NSMutableArray alloc] init];
+    
+    // Initialize the root of our Firebase namespace.
+    //self.firebase = [[Firebase alloc] initWithUrl:kFirechatNS];
+    
+    // Pick a random number between 1-1000 for our username.
+    //self.name = [NSString stringWithFormat:@"Guest%d", arc4random() % 1000];
+    NSLog(@"username %@",[[PFUser currentUser] username]);
+    
+    self.name = [[PFUser currentUser] username];
+    
+    [nameField setTitle:self.name forState:UIControlStateNormal];
+    /*
+   [self.firebase observeEventType:FEventTypeChildAdded withBlock:^(FDataSnapshot *snapshot) {
+         // Add the chat message to the array.
+       [self.chat addObject:snapshot.value];
+        // Reload the table view so the new message will show up.
+        [self.tableView reloadData];
+    }];*/
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Text field handling
+
+// This method is called when the user enters text in the text field.
+// We add the chat message to our Firebase.
+- (BOOL)textFieldShouldReturn:(UITextField*)aTextField
+{
+    [aTextField resignFirstResponder];
+
+    // This will also add the message to our local array self.chat because
+    // the FEventTypeChildAdded event will be immediately fired.
+   [[self.firebase childByAutoId] setValue:@{@"name" : self.name, @"text": aTextField.text}];
+
+    [aTextField setText:@""];
+    return NO;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView*)tableView
+{
+    // We only have one section in our table view.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView*)table numberOfRowsInSection:(NSInteger)section
+{
+    // This is the number of chat messages.
+    return [self.chat count];
+}
+
+- (UITableViewCell*)tableView:(UITableView*)table cellForRowAtIndexPath:(NSIndexPath *)index
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [table dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+    }
+    
+    NSDictionary* chatMessage = [self.chat objectAtIndex:index.row];
+    
+    cell.textLabel.text = chatMessage[@"text"];
+    cell.detailTextLabel.text = chatMessage[@"name"];
+    
+    return cell;
+}
+
+#pragma mark - Keyboard handling
+
+// Subscribe to keyboard show/hide notifications.
+- (void)viewWillAppear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self selector:@selector(keyboardWillShow:)
+        name:UIKeyboardWillShowNotification object:nil];
+
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self selector:@selector(keyboardWillHide:)
+        name:UIKeyboardWillHideNotification object:nil];
+}
+
+// Unsubscribe from keyboard show/hide notifications.
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter]
+        removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter]
+        removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
+// Setup keyboard handlers to slide the view containing the table view and
+// text field upwards when the keyboard shows, and downwards when it hides.
+- (void)keyboardWillShow:(NSNotification*)notification
+{
+    [self moveView:[notification userInfo] up:YES];
+}
+
+- (void)keyboardWillHide:(NSNotification*)notification
+{
+    [self moveView:[notification userInfo] up:NO];
+}
+
+- (void)moveView:(NSDictionary*)userInfo up:(BOOL)up
+{
+    CGRect keyboardEndFrame;
+    [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey]
+     getValue:&keyboardEndFrame];
+    
+    UIViewAnimationCurve animationCurve;
+    [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey]
+     getValue:&animationCurve];
+    
+    NSTimeInterval animationDuration;
+    [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey]
+     getValue:&animationDuration];
+    
+    // Get the correct keyboard size to we slide the right amount.
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView setAnimationCurve:animationCurve];
+    
+    CGRect keyboardFrame = [self.view convertRect:keyboardEndFrame toView:nil];
+    int y = keyboardFrame.size.height * (up ? -1 : 1);
+    self.view.frame = CGRectOffset(self.view.frame, 0, y);
+    
+    [UIView commitAnimations];
+}
+
+// This method will be called when the user touches on the tableView, at
+// which point we will hide the keyboard (if open). This method is called
+// because UITouchTableView.m calls nextResponder in its touch handler.
+- (void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)event
+{
+    if ([textField isFirstResponder]) {
+        [textField resignFirstResponder];
+    }
+}
+
+@end
